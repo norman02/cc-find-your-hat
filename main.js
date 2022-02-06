bf5eafb5c25e3341fcf8f0ae9a60b2ff97173c51
John N. Norman <normanj777@gmail.com>
2022-02-06 11:39:33 -0500

    create field class

--- main.js
+++ main.js
@@ -1,7 +1,65 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
 
+class Field {
+  constructor(arr) {
+    this._field = arr;
+  }
+  print() {
+    this._field.forEach((e) => {
+      console.log(e.join(""));
+    });
+  }
+}
+const blankArray = [
+  [
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+    fieldCharacter,
+  ],
+];
+
+const fieldArray = [blankArray, blankArray, blankArray]
+
+const myField = new Field(fieldArray);
+
+myField.print();
